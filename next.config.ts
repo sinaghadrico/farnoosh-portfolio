d1d7440fa4c05a96f4c9d9f4c13419c41b378554
Sina Ghadri <info@sinaghadri.com>
2026-04-30 22:08:38 +0400

    chore: Add .nvmrc file for Node.js version management and update package.json to specify Node.js engine and include @opennextjs/cloudflare dependency

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  output: "standalone",
   compress: true,
   images: {
     formats: ["image/avif", "image/webp"],
